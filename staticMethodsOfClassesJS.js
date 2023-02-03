e185209edbfaea6da88c58f9b7dbef8e8e1e1edc
Elyzavetka <lizamybella@gmail.com>
2023-02-03 23:49:08 +0000

    Create staticMethodsOfClassesJS.js

---
+++ staticMethodsOfClassesJS.js
@@ -0,0 +1,13 @@
+class Comment {
+  constructor(text) {
+    this.text = text;
+    this.votesQty = 0;
+  }
+
+  upvote() {
+    this.votesQty += 1;
+  }
+  static mergeComments(first, second) {
+    return `${first} ${second}`;
+  }
+}
